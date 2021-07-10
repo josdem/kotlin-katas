d067fef35deeaa3351e92b603c1521d9ce7bc4e9
josdem <joseluis.delacruz@gmail.com>
2021-07-10 11:35:42 -0400

    Adding disemvowels kata solution

--- disemvowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
+++ disemvowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
@@ -1,5 +1,17 @@
 package com.jos.dem.kata
 
+import java.util.stream.Collectors
+
 class VowelTrollRemover {
-    fun remove(keyword: String) = keyword
+
+    fun remove(keyword: String) : String {
+        return keyword
+            .chars()
+            .mapToObj { ch -> ch.toChar() }
+            .filter { ch ->
+                ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' && ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U'
+            }
+            .map { ch -> ch.toString() }
+            .collect(Collectors.joining())
+    }
 }
