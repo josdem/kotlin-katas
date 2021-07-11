6335e0f76f3499c9a4b71b5bc14785bb25f39995
josdem <joseluis.delacruz@gmail.com>
2021-07-11 10:32:42 -0400

    Adding credit card mask solution

--- credit-card-mask/src/main/kotlin/com/jos/dem/kata/Maskarator.kt
+++ credit-card-mask/src/main/kotlin/com/jos/dem/kata/Maskarator.kt
@@ -2,11 +2,8 @@ package com.jos.dem.kata
 
 class Maskarator {
 
-    private val SUFFIX_LENGTH = 4
-
     fun maskify(keyword: String): String {
-        return if (keyword.length <= SUFFIX_LENGTH) keyword else keyword.substring(0, keyword.length - SUFFIX_LENGTH)
-            .replace(".".toRegex(), "#") + keyword.substring(keyword.length - SUFFIX_LENGTH)
+        return keyword.replace(".(?=.{4})".toRegex(), "#")
     }
 
 }
