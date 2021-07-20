5ef44902cbe6776e1ac2ada345b098b5d38ee44c
josdem <joseluis.delacruz@gmail.com>
2021-07-20 09:00:10 -0400

    Adding find the odd kata

---
+++ find-the-odd/src/main/kotlin/com/jos/dem/kata/OddFinder.kt
@@ -0,0 +1,8 @@
+package com.jos.dem.kata
+
+class OddFinder {
+    fun findIt(arrayOf: IntArray): Int {
+        return 0
+    }
+
+}
---
+++ find-the-odd/src/test/kotlin/com/jos/dem/kata/OddFinderTest.kt
@@ -0,0 +1,22 @@
+package com.jos.dem.kata
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class OddFinderTest {
+
+    private val oddFinder: OddFinder = OddFinder()
+
+    @Test
+    fun `should find the odd`(){
+        assertEquals(
+            5, oddFinder.findIt(intArrayOf(20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5))
+        )
+        assertEquals(-1, oddFinder.findIt(intArrayOf(1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5)))
+        assertEquals(5, oddFinder.findIt(intArrayOf(20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5)))
+        assertEquals(10, oddFinder.findIt(intArrayOf(10)))
+        assertEquals(10, oddFinder.findIt(intArrayOf(1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1)))
+        assertEquals(1, oddFinder.findIt(intArrayOf(5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10)))
+
+    }
+}
