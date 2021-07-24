9c66fa7129fcf5f1d91d8e14833f701f7c08a71d
josdem <joseluis.delacruz@gmail.com>
2021-07-23 21:35:14 -0400

    Adding find the odd kata solution

--- find-the-odd/src/main/kotlin/com/jos/dem/kata/OddFinder.kt
+++ find-the-odd/src/main/kotlin/com/jos/dem/kata/OddFinder.kt
@@ -1,8 +1,13 @@
 package com.jos.dem.kata
 
+/*
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+ */
+
 class OddFinder {
-    fun findIt(arrayOf: IntArray): Int {
-        return 0
+    fun findIt(array: IntArray): Int {
+        return array.groupBy { it }.entries.first { it.value.size % 2 == 1 }.key
     }
 
 }
