bc64ff7237c5a682ac6e67c6c1955191b25a51b6
josdem <joseluis.delacruz@gmail.com>
2021-08-14 19:50:15 -0400

    Adding sum a collection kata

---
+++ collection-sum/src/main/kotlin/com/jos/dem/kata/CollectionAdder.kt
@@ -0,0 +1,12 @@
+package com.jos.dem.kata
+
+/*
+Given an integer collection return a sum of its elements
+ */
+
+class CollectionAdder {
+    fun sum(numbers: List<Int>): Int {
+        return numbers.size
+    }
+
+}
---
+++ collection-sum/src/test/kotlin/com/jos/dem/kata/CollectionAdderTest.kt
@@ -0,0 +1,19 @@
+package com.jos.dem.kata
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CollectionAdderTest {
+
+    private val collectionAdder: CollectionAdder = CollectionAdder()
+
+    @Test
+    @DisplayName("Accumulating a collection")
+    fun `should sum a collection`() {
+        assertEquals(31, collectionAdder.sum(listOf(1, 2, 3, 4, 10, 11)));
+        assertEquals(21, collectionAdder.sum(listOf(5, 4, 1, 2, 9)));
+    }
+
+
+}
