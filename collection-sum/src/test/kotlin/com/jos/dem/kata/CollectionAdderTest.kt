81e94e2847ba4566b3cdccaf9742e484700af619
josdem <joseluis.delacruz@gmail.com>
2021-08-14 19:51:57 -0400

    Deleting unused semicolon

--- collection-sum/src/test/kotlin/com/jos/dem/kata/CollectionAdderTest.kt
+++ collection-sum/src/test/kotlin/com/jos/dem/kata/CollectionAdderTest.kt
@@ -11,8 +11,8 @@ class CollectionAdderTest {
     @Test
     @DisplayName("Accumulating a collection")
     fun `should sum a collection`() {
-        assertEquals(31, collectionAdder.sum(listOf(1, 2, 3, 4, 10, 11)));
-        assertEquals(21, collectionAdder.sum(listOf(5, 4, 1, 2, 9)));
+        assertEquals(31, collectionAdder.sum(listOf(1, 2, 3, 4, 10, 11)))
+        assertEquals(21, collectionAdder.sum(listOf(5, 4, 1, 2, 9)))
     }
 
 
