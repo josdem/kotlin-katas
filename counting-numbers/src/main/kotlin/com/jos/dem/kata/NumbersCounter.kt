c8faa5eeb8ec7d5acc508d32bf718166432c744d
josdem <joseluis.delacruz@gmail.com>
2021-08-30 21:50:02 -0500

    Adding counting numbers kata solution

--- counting-numbers/src/main/kotlin/com/jos/dem/kata/NumbersCounter.kt
+++ counting-numbers/src/main/kotlin/com/jos/dem/kata/NumbersCounter.kt
@@ -3,13 +3,11 @@ package com.jos.dem.kata
 /*
 Given an integer collection, return an array with three elements:
 how many of them are positive, how many negative and how many are zeros.
-Given: [-4,3,-9,0,4,1] then expected output is: [3, 2, 1]
+Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
  */
 
 class NumbersCounter {
 
-    fun count(numbers: IntArray): IntArray? {
-        return numbers
-    }
+    fun count(numbers: IntArray): IntArray? = intArrayOf(numbers.count { it > 0 }, numbers.count { it < 0 }, numbers.count {it == 0})
 
 }
