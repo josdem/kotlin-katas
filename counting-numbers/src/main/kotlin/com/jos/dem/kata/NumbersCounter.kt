9fbc95c293dc3b08b53c10ec3ce50feb2229a9c2
josdem <joseluis.delacruz@gmail.com>
2021-08-31 07:01:27 -0500

    Avoiding unused thread safe

--- counting-numbers/src/main/kotlin/com/jos/dem/kata/NumbersCounter.kt
+++ counting-numbers/src/main/kotlin/com/jos/dem/kata/NumbersCounter.kt
@@ -8,6 +8,6 @@ Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
 
 class NumbersCounter {
 
-    fun count(numbers: IntArray): IntArray? = intArrayOf(numbers.count { it > 0 }, numbers.count { it < 0 }, numbers.count {it == 0})
+    fun count(numbers: IntArray): IntArray = intArrayOf(numbers.count { it > 0 }, numbers.count { it < 0 }, numbers.count {it == 0})
 
 }
