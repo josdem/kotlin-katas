bc45c8a219da2727ffd30293786e674742c7fe2a
josdem <joseluis.delacruz@gmail.com>
2021-11-07 19:42:51 -0500

    Adding common elements kata

---
+++ common-elements/src/main/java/com/jos/dem/kata/CommonElements.kt
@@ -0,0 +1,15 @@
+package com.jos.dem.kata;
+
+/*
+Given two arrays with integers.
+Then I want to return elements in common.
+Input: [1,2,3,4,5] and [1,3,5,7,9]
+Output: [1, 3 ,5]
+*/
+
+class CommonElements {
+
+  fun find(first: List<Int>, second: List<Int>): List<Int> {
+    return first
+  }
+}
---
+++ common-elements/src/test/java/com/jos/dem/kata/CommonElementsTest.kt
@@ -0,0 +1,18 @@
+package com.jos.dem.kata;
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CommonElementsTest {
+
+  private val commonElements: CommonElements = CommonElements()
+
+  @Test
+  @DisplayName("find common elements")
+  fun shouldFindCommonElements() {
+    assertEquals(
+        listOf(1, 3, 5),
+        commonElements.find(listOf(1, 2, 3, 4, 5), listOf(1, 3, 5, 7, 9)));
+  }
+}
