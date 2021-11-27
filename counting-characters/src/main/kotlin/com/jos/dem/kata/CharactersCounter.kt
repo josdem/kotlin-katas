35df130c36c25bb7a5b6feb8696f685e17fc7bdf
josdem <joseluis.delacruz@gmail.com>
2021-11-26 18:22:52 -0600

    Moving vowels and consonants values as class level

--- counting-characters/src/main/kotlin/com/jos/dem/kata/CharactersCounter.kt
+++ counting-characters/src/main/kotlin/com/jos/dem/kata/CharactersCounter.kt
@@ -1,17 +1,18 @@
 package com.jos.dem.kata
 
 class CharactersCounter {
+    
+    private val vowels = listOf('a', 'e', 'i', 'o', 'u')
+    private val consonants = listOf(
+        'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'r', 'p', 'q', 's', 't', 'v',
+        'w', 'x', 'y', 'z'
+    )
 
     fun countVowels(keyword: String): Int {
-        val vowels = listOf('a', 'e', 'i', 'o', 'u')
         return keyword.count { vowels.contains(it) }
     }
 
     fun countConsonants(keyword: String): Int {
-        val consonants = listOf(
-            'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'r', 'p', 'q', 's', 't', 'v',
-            'w', 'x', 'y', 'z'
-        )
         return keyword.count { consonants.contains(it) }
     }
 
