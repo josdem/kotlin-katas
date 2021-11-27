ffdb1d738ff10beb00d6399486be54f096a678db
josdem <joseluis.delacruz@gmail.com>
2021-11-26 18:09:15 -0600

    Adding description in the test case

--- counting-characters/src/test/kotlin/com/jos/dem/kata/CharactersCounterTest.kt
+++ counting-characters/src/test/kotlin/com/jos/dem/kata/CharactersCounterTest.kt
@@ -1,5 +1,12 @@
 package com.jos.dem.kata
 
+/*
+* Counting vowels and consonants. Create two functions one for counting vowels and another for counting consonants
+* Given: A string
+* When: That string is josdem
+* Then: Counting vowels should be 2 and consonants 4
+*/
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
