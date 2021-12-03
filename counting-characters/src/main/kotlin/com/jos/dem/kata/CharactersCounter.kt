65569210aa4fa514f6e61a80953a6f2a8d208c5e
josdem <joseluis.delacruz@gmail.com>
2021-12-03 08:51:38 -0600

    Simplifying functions

--- counting-characters/src/main/kotlin/com/jos/dem/kata/CharactersCounter.kt
+++ counting-characters/src/main/kotlin/com/jos/dem/kata/CharactersCounter.kt
@@ -1,19 +1,14 @@
 package com.jos.dem.kata
 
 class CharactersCounter {
-    
+
     private val vowels = listOf('a', 'e', 'i', 'o', 'u')
     private val consonants = listOf(
         'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'r', 'p', 'q', 's', 't', 'v',
         'w', 'x', 'y', 'z'
     )
 
-    fun countVowels(keyword: String): Int {
-        return keyword.count { vowels.contains(it) }
-    }
-
-    fun countConsonants(keyword: String): Int {
-        return keyword.count { consonants.contains(it) }
-    }
+    fun countVowels(keyword: String): Int = keyword.count { vowels.contains(it) }
+    fun countConsonants(keyword: String): Int = keyword.count { consonants.contains(it) }
 
 }
