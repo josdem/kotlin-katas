b1177d71e53a169900e795c88b15881374ad2baf
josdem <joseluis.delacruz@gmail.com>
2022-01-04 14:28:22 -0600

    Adding smallest and biggest kata

---
+++ smallest-biggest/src/main/kotlin/com/jos/dem/kata/SmallestBiggest.kt
@@ -0,0 +1,16 @@
+package com.jos.dem.kata
+
+/*
+Find smaller and biggest numbers in a collection
+Given: A collection like [7, 5, 2, 4, 3, 9]
+When: I call find method
+Then: I will get a collection with [2, 9] values
+ */
+
+class SmallestBiggest {
+
+    fun find(numbers: List<Number>): List<Number> {
+        return numbers
+    }
+
+}
---
+++ smallest-biggest/src/test/kotlin/com/jos/dem/kata/SmallestBiggestTest.kt
@@ -0,0 +1,15 @@
+package com.jos.dem.kata
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SmallestBiggestTest {
+
+    val smallestBiggest : SmallestBiggest = SmallestBiggest()
+
+    @Test
+    fun `should find smallest and biggest`(){
+        val numbers = listOf<Number>(7, 5, 2, 4, 3, 9)
+        assertEquals(listOf(2, 9), smallestBiggest.find(numbers))
+    }
+}
