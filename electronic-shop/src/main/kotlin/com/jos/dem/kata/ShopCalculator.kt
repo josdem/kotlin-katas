dd6423498808ad47c55f42709fa4d6ca3734a0c1
josdem <joseluis.delacruz@gmail.com>
2022-01-06 13:53:13 -0600

    Adding electronic shop kata solution

--- electronic-shop/src/main/kotlin/com/jos/dem/kata/ShopCalculator.kt
+++ electronic-shop/src/main/kotlin/com/jos/dem/kata/ShopCalculator.kt
@@ -1,8 +1,13 @@
 package com.jos.dem.kata
 
+import java.util.stream.Collectors
+
 class ShopCalculator {
     fun compute(amount: Int, keyboards: List<Int>, usbs: List<Int>): Int {
-        TODO("Not yet implemented")
+        val pairs = ArrayList<Pair<Int, Int>>()
+        keyboards.forEach { k -> usbs.forEach { u -> pairs.add(Pair(k, u)) } }
+        val prices = pairs.stream().map { pair -> pair.first + pair.second }.collect(Collectors.toList())
+        return prices.stream().filter { it <= amount }.max(Int::compareTo).get()
     }
 
 }
