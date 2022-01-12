fd56d18e25407acf2eeb89f919641d83fee095ce
josdem <joseluis.delacruz@gmail.com>
2022-01-11 18:00:51 -0600

    Adding square my list kata solution

--- square-calculator/src/main/kotlin/com/jos/dem/kata/SquareCalculator.kt
+++ square-calculator/src/main/kotlin/com/jos/dem/kata/SquareCalculator.kt
@@ -1,8 +1,5 @@
 package com.jos.dem.kata
 
 class SquareCalculator {
-    fun square(numbers: List<Int>): List<Int> {
-        return numbers
-    }
-
+    fun square(numbers: List<Int>): List<Int> = numbers.map { it * it }
 }
