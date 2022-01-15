9748d7b798c3e05b1078f9af9910867de73ffb25
josdem <joseluis.delacruz@gmail.com>
2022-01-15 10:37:38 -0600

    Getting list as strings

--- digital-sum/src/main/kotlin/com/jos/dem/kata/DigitalAdder.kt
+++ digital-sum/src/main/kotlin/com/jos/dem/kata/DigitalAdder.kt
@@ -1,7 +1,11 @@
 package com.jos.dem.kata
 
+import java.lang.String
+import kotlin.Int
+
 class DigitalAdder {
     fun add(numbers: List<Int>): List<Int> {
+        val numbersAsString = numbers.map { String.valueOf(it) }
         return numbers
     }
 
