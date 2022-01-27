81007fcb0a01669d348e559cdeb3d27561b98e8d
josdem <joseluis.delacruz@gmail.com>
2022-01-26 18:39:19 -0600

    Formatting code

--- counting-character/src/main/kotlin/com/jos/dem/kata/CharacterCounter.kt
+++ counting-character/src/main/kotlin/com/jos/dem/kata/CharacterCounter.kt
@@ -2,6 +2,8 @@ package com.jos.dem.kata
 
 class CharacterCounter {
 
-    fun count(keyword: String, ch: Char): Int = keyword.chars().filter { ch === it.toChar() }.count().toInt()
+    fun count(keyword: String, ch: Char): Int {
+        return keyword.length + ch.code
+    }
 
 }
