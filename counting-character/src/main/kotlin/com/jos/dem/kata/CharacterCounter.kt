0c3e37a9383bafb031c55787637c912d8980d67e
josdem <joseluis.delacruz@gmail.com>
2022-01-26 18:39:51 -0600

    Adding counting character kata solution

--- counting-character/src/main/kotlin/com/jos/dem/kata/CharacterCounter.kt
+++ counting-character/src/main/kotlin/com/jos/dem/kata/CharacterCounter.kt
@@ -2,8 +2,6 @@ package com.jos.dem.kata
 
 class CharacterCounter {
 
-    fun count(keyword: String, ch: Char): Int {
-        return keyword.length + ch.code
-    }
+    fun count(keyword: String, ch: Char): Int = keyword.chars().filter { ch === it.toChar() }.count().toInt()
 
 }
