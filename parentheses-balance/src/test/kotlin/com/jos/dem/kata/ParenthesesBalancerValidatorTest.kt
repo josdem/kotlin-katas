230563e2ba8cf364f3f1264ded5e707273eb00c3
josdem <joseluis.delacruz@gmail.com>
2022-02-03 12:22:07 -0600

    Adding parentheses balance kata

---
+++ parentheses-balance/src/test/kotlin/com/jos/dem/kata/ParenthesesBalancerValidatorTest.kt
@@ -0,0 +1,28 @@
+package com.jos.dem.kata
+
+/**
+ *  Given a string
+ *  When I call isBalanced method
+ *  Then Return a boolean indicating whether that string has balanced parentheses.
+ */
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ParenthesesBalancerValidatorTest {
+
+    val validator: ParenthesesBalancerValidator = ParenthesesBalancerValidator()
+
+    @Test
+    fun `should check if keyword is balanced`() {
+        val keyword = "ab(c)(d())"
+        assertTrue(validator.isBalanced(keyword), "string is balanced")
+    }
+
+    @Test
+    fun `should validate keyword is not balanced`() {
+        val keyword = "(x()())"
+        assertFalse(validator.isBalanced(keyword), "string is not balanced")
+    }
+}
