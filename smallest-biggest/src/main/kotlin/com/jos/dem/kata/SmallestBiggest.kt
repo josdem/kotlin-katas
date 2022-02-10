b81c01dec39c55180535b0a076e2cc0718e32768
josdem <joseluis.delacruz@gmail.com>
2022-02-10 13:05:41 -0500

    Adding smallest biggest kata solution

--- smallest-biggest/src/main/kotlin/com/jos/dem/kata/SmallestBiggest.kt
+++ smallest-biggest/src/main/kotlin/com/jos/dem/kata/SmallestBiggest.kt
@@ -3,7 +3,9 @@ package com.jos.dem.kata
 class SmallestBiggest {
 
     fun find(numbers: List<Int>): List<Int> {
-        TODO("Not implemented yet")
+        val smallest = numbers.minOrNull()
+        val biggest = numbers.maxOrNull()
+        return listOf(smallest ?: 0, biggest ?: 0)
     }
 
 }
