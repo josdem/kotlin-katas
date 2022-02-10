595dd6c662134a5ff509297f3f22e1f74990dfa5
josdem <joseluis.delacruz@gmail.com>
2022-02-10 13:04:02 -0500

    Fixing smallest biggest kata

--- smallest-biggest/src/main/kotlin/com/jos/dem/kata/SmallestBiggest.kt
+++ smallest-biggest/src/main/kotlin/com/jos/dem/kata/SmallestBiggest.kt
@@ -1,18 +1,9 @@
 package com.jos.dem.kata
 
-/*
-Find smaller and biggest numbers in a collection
-Given: A collection like [7, 5, 2, 4, 3, 9]
-When: I call find method
-Then: I will get a collection with [2, 9] values
- */
-
 class SmallestBiggest {
 
     fun find(numbers: List<Int>): List<Int> {
-        val smallest = numbers.minOrNull()
-        val biggest = numbers.maxOrNull()
-        return listOf(smallest ?: 0, biggest ?: 0)
+        TODO("Not implemented yet")
     }
 
 }
