6a21146b3317ec3370dd91788aa64c8c0b2f989b
josdem <joseluis.delacruz@gmail.com>
2022-02-23 16:40:06 -0500

    Adding array diff kata

--- array-diff/src/main/kotlin/com/jos/dem/kata/Differentiator.kt
+++ array-diff/src/main/kotlin/com/jos/dem/kata/Differentiator.kt
@@ -3,7 +3,7 @@ package com.jos.dem.kata
 class Differentiator {
 
     fun diff(a: IntArray, b: IntArray): IntArray {
-        return a.plus(b)
+        return a.filter { !b.contains(it) }.toIntArray()
     }
 
 }
