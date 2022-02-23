909bfb13f89bafa38e8060b4ef5cd68a3c206683
josdem <joseluis.delacruz@gmail.com>
2022-02-23 16:35:20 -0500

    Deleting implementation

--- array-diff/src/test/kotlin/com/jos/dem/kata/DifferentiatorTest.kt
+++ array-diff/src/test/kotlin/com/jos/dem/kata/DifferentiatorTest.kt
@@ -1,5 +1,10 @@
 package com.jos.dem.kata
 
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b keeping their order.
+*/
+
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 
@@ -8,7 +13,7 @@ class DifferentiatorTest {
     private val differentiator: Differentiator = Differentiator()
 
     @Test
-    fun `should get difference in arrays`(){
+    fun `should get difference in arrays`() {
         assertArrayEquals(intArrayOf(2), differentiator.diff(intArrayOf(1, 2), intArrayOf(1)))
         assertArrayEquals(intArrayOf(2, 2), differentiator.diff(intArrayOf(1, 2, 2), intArrayOf(1)))
         assertArrayEquals(intArrayOf(1), differentiator.diff(intArrayOf(1, 2, 2), intArrayOf(2)))
