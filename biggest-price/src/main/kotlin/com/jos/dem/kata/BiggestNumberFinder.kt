7f1cdbc5b8f4f821de1c146c6dd173d557f60a9b
josdem <joseluis.delacruz@gmail.com>
2022-02-24 09:08:34 -0500

    Removing implementation from master

--- biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
+++ biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
@@ -1,18 +1,11 @@
 package com.jos.dem.kata
 
-/*
-Messages with random data are coming! But we just care about prices!
-Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
-messages = ["hi", "2.0", "@#$%", "32.0"]
-result = 32.0
-*/
-
 class BiggestNumberFinder {
 
     private val pattern = "-?[0-9]+.?[0-9]+"
 
     fun find(data: List<String>): Double {
-        return data.filter { it.matches( pattern.toRegex()) }
+        return data.filter { it.matches(pattern.toRegex()) }
             .map { it.toDouble() }.maxOrNull() ?: 0.0
     }
 
--- biggest-price/src/test/kotlin/com/jos/dem/kata/BiggestNumberFinderTest.kt
+++ biggest-price/src/test/kotlin/com/jos/dem/kata/BiggestNumberFinderTest.kt
@@ -1,11 +1,18 @@
 package com.jos.dem.kata
 
+/*
+Messages with random data are coming! But we care about prices!
+Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
+messages = ["hi", "2.0", "@#$%", "32.0"]
+result = 32.0
+*/
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class BiggestNumberFinderTest {
 
-    private val biggestNumberFinder : BiggestNumberFinder = BiggestNumberFinder()
+    private val biggestNumberFinder: BiggestNumberFinder = BiggestNumberFinder()
 
     @Test
     fun `should find biggest number`() {
