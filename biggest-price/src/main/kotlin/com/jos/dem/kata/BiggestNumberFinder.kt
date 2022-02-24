a669de104be9a6b06c945093db69993e99a95826
josdem <joseluis.delacruz@gmail.com>
2022-02-24 09:10:00 -0500

    Removing implementation from master

--- biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
+++ biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
@@ -2,11 +2,8 @@ package com.jos.dem.kata
 
 class BiggestNumberFinder {
 
-    private val pattern = "-?[0-9]+.?[0-9]+"
-
     fun find(data: List<String>): Double {
-        return data.filter { it.matches(pattern.toRegex()) }
-            .map { it.toDouble() }.maxOrNull() ?: 0.0
+        return data.size.toDouble()
     }
 
 }
