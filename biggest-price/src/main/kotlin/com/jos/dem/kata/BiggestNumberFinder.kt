4ac312f5ea93f47734c1e9712167b74598cde1e0
josdem <joseluis.delacruz@gmail.com>
2022-02-24 09:13:18 -0500

    Merging max with maxOfOrNull

--- biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
+++ biggest-price/src/main/kotlin/com/jos/dem/kata/BiggestNumberFinder.kt
@@ -5,8 +5,7 @@ class BiggestNumberFinder {
     private val pattern = "-?[0-9]+.?[0-9]+"
 
     fun find(data: List<String>): Double {
-        return data.filter { it.matches(pattern.toRegex()) }
-            .map { it.toDouble() }.maxOrNull() ?: 0.0
+        return data.filter { it.matches(pattern.toRegex()) }.maxOfOrNull { it.toDouble() } ?: 0.0
     }
 
 }
