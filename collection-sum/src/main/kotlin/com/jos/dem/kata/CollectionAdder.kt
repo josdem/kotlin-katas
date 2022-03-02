535051ab25ec915356c679422d5c3dddeda618bd
josdem <joseluis.delacruz@gmail.com>
2022-03-02 16:17:14 -0500

    Deleting implementation from master

--- collection-sum/src/main/kotlin/com/jos/dem/kata/CollectionAdder.kt
+++ collection-sum/src/main/kotlin/com/jos/dem/kata/CollectionAdder.kt
@@ -1,12 +1,8 @@
 package com.jos.dem.kata
 
-/*
-Given an integer collection return a sum of its elements
- */
-
 class CollectionAdder {
     fun sum(numbers: List<Int>): Int {
-        return numbers.sum()
+        return numbers.size
     }
 
 }
