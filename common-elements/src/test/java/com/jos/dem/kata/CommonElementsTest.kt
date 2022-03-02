5827ea8ea9844048899360bc6be6d355f5895418
josdem <joseluis.delacruz@gmail.com>
2022-03-02 16:27:36 -0500

    Removing implementation from master

--- common-elements/src/test/java/com/jos/dem/kata/CommonElementsTest.kt
+++ common-elements/src/test/java/com/jos/dem/kata/CommonElementsTest.kt
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
+/*
+Given two arrays with integers.
+Then I want to return elements in common.
+Input: [1,2,3,4,5] and [1,3,5,7,9]
+Output: [1, 3 ,5]
+*/
+
 class CommonElementsTest {
 
     private val commonElements: CommonElements = CommonElements()
