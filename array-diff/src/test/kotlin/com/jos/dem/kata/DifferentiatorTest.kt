acb6f3615369a40d1d980b3e45376617442d359a
josdem <joseluis.delacruz@gmail.com>
2022-03-03 12:52:47 -0500

    Fixing grammar

--- array-diff/src/test/kotlin/com/jos/dem/kata/DifferentiatorTest.kt
+++ array-diff/src/test/kotlin/com/jos/dem/kata/DifferentiatorTest.kt
@@ -2,7 +2,7 @@ package com.jos.dem.kata
 
 /*
 Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-It should remove all values from list a, which are present in list b keeping their order.
+It should remove all values from list a, which are present in list b, keeping their order.
 */
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
