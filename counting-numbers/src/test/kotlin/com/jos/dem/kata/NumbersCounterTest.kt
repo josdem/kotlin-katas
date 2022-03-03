b3d2726e67579795f1eddc75ff0bd7166d405435
josdem <joseluis.delacruz@gmail.com>
2022-03-02 20:36:29 -0500

    Removing implementation from master

--- counting-numbers/src/test/kotlin/com/jos/dem/kata/NumbersCounterTest.kt
+++ counting-numbers/src/test/kotlin/com/jos/dem/kata/NumbersCounterTest.kt
@@ -3,9 +3,15 @@ package com.jos.dem.kata
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 
+/*
+Given an integer collection, return an array with three elements:
+how many of them are positive, how many negatives, and how many zeros.
+Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
+ */
+
 class NumbersCounterTest {
 
-    private val numbersCounter : NumbersCounter = NumbersCounter()
+    private val numbersCounter: NumbersCounter = NumbersCounter()
 
     @Test
     fun `should count numbers`() {
