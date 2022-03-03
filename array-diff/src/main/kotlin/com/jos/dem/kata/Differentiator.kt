622ffea764dcc81dec2a344e8bdfd5b39a6c0ea8
josdem <joseluis.delacruz@gmail.com>
2022-03-03 12:58:19 -0500

    Returning implementation to develop

--- array-diff/src/main/kotlin/com/jos/dem/kata/Differentiator.kt
+++ array-diff/src/main/kotlin/com/jos/dem/kata/Differentiator.kt
@@ -2,8 +2,7 @@ package com.jos.dem.kata
 
 class Differentiator {
 
-    fun diff(a: IntArray, b: IntArray): IntArray {
-        return a.filter { !b.contains(it) }.toIntArray()
-    }
+    fun diff(a: IntArray, b: IntArray): IntArray = a.filter { !b.contains(it) }.toIntArray()
+    
 
 }
