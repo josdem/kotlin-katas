d392e1bc1e0f7402fff10468c83b9a3855abae98
josdem <joseluis.delacruz@gmail.com>
2022-03-10 08:33:51 -0500

    Removing implementation from master

--- create-phone-number/src/test/kotlin/com/jos/dem/kata/PhoneNumberGeneratorTest.kt
+++ create-phone-number/src/test/kotlin/com/jos/dem/kata/PhoneNumberGeneratorTest.kt
@@ -1,14 +1,21 @@
 package com.jos.dem.kata
 
+/*
+Write a function that accepts an array of 10 positive integers and returns a string with those numbers in phone number format.
+Given [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+When I call format method
+Then I validate phone number is (123) 456-7890
+ */
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class PhoneNumberGeneratorTest {
 
-    private val phoneNumberGenerator : PhoneNumberGenerator = PhoneNumberGenerator()
+    private val phoneNumberGenerator: PhoneNumberGenerator = PhoneNumberGenerator()
 
     @Test
-    fun `should format phone number`(){
+    fun `should format phone number`() {
         assertEquals(
             "(123) 456-7890", phoneNumberGenerator.format(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0))
         )
