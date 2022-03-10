21a6abc91e22e33db1a5e18322c6f157d0c72a8f
josdem <joseluis.delacruz@gmail.com>
2022-03-10 15:06:44 -0500

    Returning implementation to develop

--- disem-vowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
+++ disem-vowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
@@ -1,8 +1,17 @@
 package com.jos.dem.kata
 
+import java.util.stream.Collectors
+
 class VowelTrollRemover {
+
+    private val vowels = listOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
 
     fun remove(keyword: String): String {
         return keyword
+            .chars()
+            .mapToObj { it.toChar() }
+            .filter { !vowels.contains(it) }
+            .map { it.toString() }
+            .collect(Collectors.joining())
     }
 }
