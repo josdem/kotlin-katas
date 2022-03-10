1c2a6aa4ff2a5a256ff67dd5ae349be698669578
josdem <joseluis.delacruz@gmail.com>
2022-03-10 09:07:42 -0500

    Returning implementation to develop

--- credit-card-mask/src/main/kotlin/com/jos/dem/kata/Maskarator.kt
+++ credit-card-mask/src/main/kotlin/com/jos/dem/kata/Maskarator.kt
@@ -3,7 +3,7 @@ package com.jos.dem.kata
 class Maskarator {
 
     fun maskify(keyword: String): String {
-        return keyword
+        return keyword.replace(".(?=.{4})".toRegex(), "#")
     }
 
 }
