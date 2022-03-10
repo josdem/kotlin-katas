74c5fae1aceb2ba5e354c376e5103528d649a72b
josdem <joseluis.delacruz@gmail.com>
2022-03-10 15:23:15 -0500

    Returning implementation to develop

--- find-the-odd/src/main/kotlin/com/jos/dem/kata/OddFinder.kt
+++ find-the-odd/src/main/kotlin/com/jos/dem/kata/OddFinder.kt
@@ -2,7 +2,7 @@ package com.jos.dem.kata
 
 class OddFinder {
     fun findIt(array: IntArray): Int {
-        return array.size
+        return array.groupBy { it }.entries.first { it.value.size % 2 == 1 }.key
     }
 
 }
