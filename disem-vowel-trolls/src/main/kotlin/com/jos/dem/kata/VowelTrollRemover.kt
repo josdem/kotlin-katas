8c15bba5b633b5fa099306ac39c627e0808982f1
josdem <joseluis.delacruz@gmail.com>
2022-03-10 14:29:49 -0500

    Removing implementation from master

--- disem-vowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
+++ disem-vowel-trolls/src/main/kotlin/com/jos/dem/kata/VowelTrollRemover.kt
@@ -1,25 +1,8 @@
 package com.jos.dem.kata
 
-/*
-Trolls are attacking your comment section!
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-Your task is to write a function that takes a string and return a new string with all vowels removed.
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-Note: for this kata y isn't considered a vowel.
- */
-
-import java.util.stream.Collectors
-
 class VowelTrollRemover {
 
     fun remove(keyword: String): String {
-        val vowels = listOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-        
         return keyword
-            .chars()
-            .mapToObj { it.toChar() }
-            .filter { !vowels.contains(it) }
-            .map { it.toString() }
-            .collect(Collectors.joining())
     }
 }
--- disem-vowel-trolls/src/test/kotlin/com/jos/dem/kata/VowelTrollRemoverTest.kt
+++ disem-vowel-trolls/src/test/kotlin/com/jos/dem/kata/VowelTrollRemoverTest.kt
@@ -1,5 +1,12 @@
 package com.jos.dem.kata
 
+/*
+Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and returns a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+ */
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
