3301f5e1a3d6fa261125b322b2b668cb4f10b88a
josdem <joseluis.delacruz@gmail.com>
2022-03-13 12:29:38 -0400

    Returning implementation to develop

--- first-non-repeated/src/main/kotlin/com/jos/dem/kata/NonRepeatedFinder.kt
+++ first-non-repeated/src/main/kotlin/com/jos/dem/kata/NonRepeatedFinder.kt
@@ -2,8 +2,6 @@ package com.jos.dem.kata
 
 class NonRepeatedFinder {
 
-    fun find(numbers: List<Int>): Int {
-        return numbers.size
-    }
+    fun find(numbers: List<Int>): Int = numbers.groupBy { it }.filter { it.value.size == 1 }.keys.elementAt(0)
 
 }
