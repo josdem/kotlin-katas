5a85c12dbb6a98e4ed3f2f555177533c1f8826c0
josdem <joseluis.delacruz@gmail.com>
2022-03-13 12:27:57 -0400

    Removing implementation from master

--- first-non-repeated/src/main/kotlin/com/jos/dem/kata/NonRepeatedFinder.kt
+++ first-non-repeated/src/main/kotlin/com/jos/dem/kata/NonRepeatedFinder.kt
@@ -1,12 +1,9 @@
 package com.jos.dem.kata
 
-/*
-Given a numbers collection find fist non-repeated number in that collection
-When we have: [25, 25, 18, 18, 18, 36, 1, 2, 2, 34] first non-repeated number is 36
- */
-
 class NonRepeatedFinder {
 
-    fun find(numbers: List<Int>): Int = numbers.groupBy { it }.filter { it.value.size == 1 }.keys.elementAt(0)
+    fun find(numbers: List<Int>): Int {
+        return numbers.size
+    }
 
 }
--- first-non-repeated/src/test/kotlin/com/jos/dem/kata/NonRepeatedFinderTest.kt
+++ first-non-repeated/src/test/kotlin/com/jos/dem/kata/NonRepeatedFinderTest.kt
@@ -1,14 +1,19 @@
 package com.jos.dem.kata
 
+/*
+Given a number, collection find the first non-repeated number in that collection
+When we have: [25, 25, 18, 18, 18, 36, 1, 2, 2, 34] first non-repeated number is 36
+ */
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class NonRepeatedFinderTest {
 
-    private val nonRepeatedFinder : NonRepeatedFinder = NonRepeatedFinder()
+    private val nonRepeatedFinder: NonRepeatedFinder = NonRepeatedFinder()
 
     @Test
-    fun `should find first non repeated`(){
+    fun `should find first non repeated`() {
         assertEquals(36, nonRepeatedFinder.find(listOf(25, 25, 18, 18, 18, 36, 1, 2, 2, 34)));
     }
 
