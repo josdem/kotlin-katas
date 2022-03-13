547fafa3aaa7b93a0b1fa6ecbfca0a3fde8ff73e
josdem <joseluis.delacruz@gmail.com>
2022-03-13 16:25:17 -0400

    Returning implementation to develop

--- string-compressor/src/main/kotlin/com/jos/dem/kata/StringCompressor.kt
+++ string-compressor/src/main/kotlin/com/jos/dem/kata/StringCompressor.kt
@@ -1,7 +1,18 @@
 package com.jos.dem.kata
 
+import java.util.stream.Collectors
+
 class StringCompressor {
     fun compress(keyword: String): String {
-        return keyword
+        val map: Map<Char, Long> = keyword
+            .chars()
+            .mapToObj { it.toChar() }
+            .collect(Collectors.groupingBy({ it }, Collectors.counting()))
+        val sb = StringBuilder()
+        map.forEach { (k: Char, v: Long) ->
+            sb.append(k)
+            sb.append(v)
+        }
+        return sb.toString()
     }
 }
