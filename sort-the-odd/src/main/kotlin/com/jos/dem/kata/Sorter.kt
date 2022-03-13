ceb0706f1cf644b8bde9bb0d320a1d490c5e08c8
josdem <joseluis.delacruz@gmail.com>
2022-03-13 16:11:52 -0400

    Returning implementation to develop

--- sort-the-odd/src/main/kotlin/com/jos/dem/kata/Sorter.kt
+++ sort-the-odd/src/main/kotlin/com/jos/dem/kata/Sorter.kt
@@ -3,7 +3,10 @@ package com.jos.dem.kata
 class Sorter {
 
     fun sort(array: IntArray): IntArray {
-        return array
+        val result: ArrayList<Int> = ArrayList()
+        val odds = array.filter { it % 2 == 1 }.sorted().toMutableList()
+        array.forEach { if (it % 2 == 0) result.add(it) else result.add(odds.removeFirst()) }
+        return result.toIntArray()
     }
 
 }
