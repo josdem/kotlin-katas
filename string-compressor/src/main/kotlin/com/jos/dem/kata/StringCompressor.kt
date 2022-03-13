5f462b6b6662136f96d00e9020a28d469a57b6fc
josdem <joseluis.delacruz@gmail.com>
2022-03-13 16:24:01 -0400

    Removing implementation from master

--- string-compressor/src/main/kotlin/com/jos/dem/kata/StringCompressor.kt
+++ string-compressor/src/main/kotlin/com/jos/dem/kata/StringCompressor.kt
@@ -1,24 +1,7 @@
 package com.jos.dem.kata
 
-import java.util.stream.Collectors
-
-/*
-Given a string "aaabbbbcc"
-When we call compress method
-Then we have a compressed string like "a3b4c2"
- */
-
 class StringCompressor {
     fun compress(keyword: String): String {
-        val map: Map<Char, Long> = keyword
-            .chars()
-            .mapToObj { it.toChar() }
-            .collect(Collectors.groupingBy({ it }, Collectors.counting()))
-        val sb = StringBuilder()
-        map.forEach { (k: Char, v: Long) ->
-            sb.append(k)
-            sb.append(v)
-        }
-        return sb.toString()
+        return keyword
     }
 }
